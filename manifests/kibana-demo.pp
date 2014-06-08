6be104756ee6b9a38b315d1cde6d9861f77e4c23
Ryan Tomlinson <tomlinsonryan@gmail.com>
2014-06-08 15:01:37 +0100

    elasticsearch now working. Now for logstash

--- manifests/kibana-demo.pp
+++ manifests/kibana-demo.pp
@@ -1,14 +1,25 @@
-exec { "apt-get-update":
-    command => "/usr/bin/apt-get update",
-}
+node 'kibana-demo' {
 
-package {'curl':
-	provider => apt,
-	ensure => latest,
-	require => Exec['apt-get-update']
-}
+	include stdlib
+	exec { "apt-get-update":
+	    command => "/usr/bin/apt-get update",
+	}
 
-class {'elasticsearch':
-  version => '0.90.0',
-  require => Exec['apt-get-update'],
+	package {'redis-server':
+		provider => apt,
+		ensure => latest,
+		require => Exec['apt-get-update']
+	}
+
+	package {'curl':
+		provider => apt,
+		ensure => latest,
+		require => Exec['apt-get-update']
+	}
+
+	class {'elasticsearch':
+	  version => '0.90.0',
+	  require => Exec['apt-get-update'],
+	}
+
 }
